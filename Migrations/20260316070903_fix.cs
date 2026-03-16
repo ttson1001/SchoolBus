1ce30124852834d6b5306e6b978434801c338c15
mainguyentuantu <whoiam.10322@gmail.com>
2026-03-16 14:09:28 +0700

    Add migration

---
+++ Migrations/20260316070903_fix.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BE_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "BusStations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "BusStations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "BusStations");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "BusStations");
+        }
+    }
+}
