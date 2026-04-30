568470dfff0c92b084a7abf7dc579877d47ce9fb
nvkhang00 <nvkhang0099@gmail.com>
2026-04-30 14:22:08 +0700

    update-deoce

---
+++ Dto/BusTripProgress/BusTripProgressStationStudentDto.cs
@@ -0,0 +1,19 @@
+namespace BE_API.Dto.BusTripProgress
+{
+    public class BusTripProgressStationStudentDto
+    {
+        public long StudentId { get; set; }
+        public string StudentCode { get; set; } = null!;
+        public string StudentName { get; set; } = null!;
+        public long StationId { get; set; }
+        public string StationName { get; set; } = null!;
+        public string? PickupAddress { get; set; }
+        public double? PickupLatitude { get; set; }
+        public double? PickupLongitude { get; set; }
+        public bool HasCheckedInOnThisBus { get; set; }
+        public bool IsCurrentlyOnThisBus { get; set; }
+        public long? CurrentBusId { get; set; }
+        public string? CurrentBusLabel { get; set; }
+        public bool IsOnDifferentBusThanAssigned { get; set; }
+    }
+}
